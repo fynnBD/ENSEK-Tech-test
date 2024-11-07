765de55b6008cb8bd468c3c0ab0112c803da7fe0
fynnb <fynnb@DTN121>
2024-11-07 15:23:21 +0000

    Add project files.

---
+++ ENSEK Technical Test/Models/ToDoContext.cs
@@ -0,0 +1,6 @@
+﻿namespace ENSEK_Technical_Test.Models
+{
+    public class ToDoContext : DBContext
+    {
+    }
+}
---
+++ ENSEK Technical Test/Models/ToDoTask.cs
@@ -0,0 +1,8 @@
+﻿namespace ENSEK_Technical_Test.Models;
+
+public class ToDoTask
+{
+    private int id { get; set; }
+    private string? name { get; set; }
+    private bool isComplete { get; set; }
+}
